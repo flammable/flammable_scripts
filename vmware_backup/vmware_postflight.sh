b05a2705577ef8e3ceae33c6c6c521bfaf2732d1
Mike Solin <mike@mikesolin.com>
2016-02-09 12:38:43 -0500

    Added code comments

--- vmware_backup/vmware_postflight.sh
+++ vmware_backup/vmware_postflight.sh
@@ -3,10 +3,12 @@
 vmrun="/Applications/VMware Fusion.app/Contents/Library/vmrun"
 running_vms="/tmp/com.mikesolin.running_vms.txt"
 
+# pause/unpause - does not save the contents of RAM to disk
 while read i; do
   "${vmrun}" -T fusion unpause "$i"
 done < ${running_vms}
 
+# suspend/resume - saves the contents of RAM to disk
 # while read i; do
 #   "${vmrun}" -T fusion start "$i" gui
 # done < ${running_vms}
--- vmware_backup/vmware_preflight.sh
+++ vmware_backup/vmware_preflight.sh
@@ -11,10 +11,12 @@ running_vms="/tmp/com.mikesolin.running_vms.txt"
 # trim the first line - we don't care how many are running
 ${tail} -n +2 ${vmrun_output} > ${running_vms}
 
+# pause/unpause - does not save the contents of RAM to disk
 while read i; do
   "${vmrun}" -T fusion pause "$i"
 done < ${running_vms}
 
+# suspend/resume - saves the contents of RAM to disk
 # while read i; do
 #   "${vmrun}" -T fusion suspend "$i" soft
 # done < ${running_vms}
