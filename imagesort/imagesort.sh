f502c39551e9517cb98a584c77766a1035aed8a8
Mike Solin <mike@mikesolin.com>
2021-06-26 10:50:08 -0400

    Add HappyFace’s edit

--- imagesort/imagesort.sh
+++ imagesort/imagesort.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+shopt -s nullglob # The script spits errors if this is not set and there are, say, no *.png files.
 for image in *.jpg *.JPG *.jpeg *.JPEG *.gif *.GIF *.bmp *.BMP *.png *.PNG;
     do res=$(identify -format %wx%h\\n "$image");
     mkdir -p $res;
