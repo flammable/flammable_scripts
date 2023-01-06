4a87e8662b242af4bac1a7133c559c7e9f5f6177
Mike Solin <mike@mikesolin.com>
2023-01-06 17:49:07 -0500

    Add variables for binaries

--- imagesort/imagesort.sh
+++ imagesort/imagesort.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
+mkdir="/bin/mkdir"
+mv="/bin/mv"
+
 shopt -s nullglob # The script spits errors if this is not set and there are, say, no *.png files.
 for image in *.jpg *.JPG *.jpeg *.JPEG *.gif *.GIF *.bmp *.BMP *.png *.PNG;
-    do res=$(identify -format %wx%h\\n "$image");
-    mkdir -p $res;
-    mv "$image" $res;
+    do res=$(identify -format %wx%h\\n "${image}");
+    ${mkdir} -p ${res};
+    ${mv} "${image}" ${res};
 done
